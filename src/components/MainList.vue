e04143a226080faf55e241ba04735489c1a2600c
Phillip T <donutdeflector@protonmail.com>
2018-06-18 22:20:04 -0400

    basic setup, API displays gen1 pokemon names

---
+++ src/components/MainList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="MainList">
+    <h1>Donutdex</h1>
+    <List/>
+  </div>
+</template>
+
+<script>
+import List from "./shared/List";
+
+export default {
+  name: "MainList",
+  components: {
+    List
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
 
-Vue.config.productionTip = false
+import "./assets/css/normalize.css";
+import "./assets/css/donutdex.css";
+
+import VueLazyload from "vue-lazyload";
+
+Vue.use(VueLazyload);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
